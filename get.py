86985449ae071e8d6358046c14f926647a4a7774
Ava Sasani <ava.sasani2@gmail.com>
2026-04-08 16:02:15 -0400

    Move slack logic to redivis

--- get.py
+++ get.py
@@ -11,9 +11,6 @@ REDIVIS_API_TOKEN = os.environ["REDIVIS_API_TOKEN"]
 if not REDIVIS_API_TOKEN:
     raise RuntimeError("Missing REDIVIS_API_TOKEN environment variable.")
 
-SLACK_BOT_TOKEN = redivis.organization("TheTrace").secret("gva_slackbot_token")
-SLACK_CHANNEL_ID = "C096NBWPZA7"
-
 def get():
     
     logger.info('Getting notebooks from Redivis...')
@@ -24,36 +21,9 @@ def get():
     notebook = redivis.notebook(f"{username}.{workflow_name}.{notebook_name}")
     
     logger.info(f'Running {notebook_name} notebook...')
-    try:
-        notebook.run(wait_for_finish=True)  # Wait for the notebook to finish running
-    except Exception as e:
-        send_slack(f'Gun Sales Tracker notebook failed: {e}', True)
+    
+    notebook.run(wait_for_finish=True)  # Wait for the notebook to finish running
+    
     logger.info(f'Running {notebook_name} notebook finished.')
     # Wordpress triggers here or in Redivis notebook itself.
-    send_slack("TEST - Gun Sales Tracker data pulled successfully: https://nics-data.s3.amazonaws.com/nics-latest.csv")
-
-def send_slack(text, error=False):
-    color = '#FFFFFF'
-    if error:
-        color = '#FF0000'
-    try:
-        response = requests.post(
-            "https://slack.com/api/chat.postMessage",
-            headers={"Authorization": f"Bearer {SLACK_BOT_TOKEN.get_value()}"},
-            json={
-                "channel": SLACK_CHANNEL_ID,
-                "text": "Gun Sales Tracker",
-                "attachments": [
-                    {
-                        "color": color,
-                        "text": text
-                    }
-                ]
-            }
-        )
-        response.raise_for_status()
-        data = response.json()
-        if not data.get("ok"):
-            raise ValueError(f"Slack API error: {data}")
-    except Exception as e:
-        logger.info(f"[Slack] Failed to send message: {e}")
+    
